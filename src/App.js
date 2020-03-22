c23eb6c53d4a803d0ee032fef8eb6fa3651a4630
iskunalnayyar <iskunalnayyar@gmail.com>
2020-03-22 19:29:29 -0400

    Move main.js contents into index.html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl'
-import Main from './components/main';
-import {Link} from 'react-router-dom';
+// import Main from './components/main';
+import LandingPage from './landingpage';
+import Contact from './contact';
+import Resume from './resume';
+
+// holds routing for all pages
+import {Switch, Route, Link} from 'react-router-dom';
 
 
 function App() {
@@ -11,22 +16,24 @@ function App() {
       <Layout>
         <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">My portfolio</Link>} scroll>
           <Navigation>
-          <Link to="/resume">Resumé</Link>
-          <Link to="/contact">Contact Me</Link>
-
+            <Link to="/resume">Resume</Link>
+            <Link to="/contact">Contact Me</Link>
           </Navigation>
         </Header>
         <Drawer title={<Link style={{textDecoration: 'none', color: 'Orange'}} to="/">My portfolio</Link>}>
           <Navigation>
-          <Link to="/resume">Resumé</Link>
-          <Link to="/contact">Contact Me</Link>
-
+            <Link to="/resume">Resume</Link>
+            <Link to="/contact">Contact Me</Link>
           </Navigation>
         </Drawer>
-
         <Content>
           <div className="page-content"/>
-          <Main/>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/resume" component={Resume} />
+          </Switch>
+          // <Main/>
         </Content>
       </Layout>
     </div>
